85f863a4fab646027cd40ed20558e7f89d3edaf5
aliamirhassani <Ali.Amirhassani1@gmail.com>
2022-11-09 17:44:23 -0800

    test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
+var temp = "Ali";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. {temp}
         </p>
         <a
           className="App-link"
