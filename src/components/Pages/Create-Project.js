6a1c50e5c6be24052d6d9692704069a263c1fcb3
birdsnbears <louischu15@gmail.com>
2022-12-03 17:02:22 -0800

    Successfully upload and retrieve images

--- src/components/Pages/Create-Project.js
+++ src/components/Pages/Create-Project.js
@@ -1,54 +1,50 @@
 import "./Create-Project.css";
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router';
-import { Get } from '../../scripts';
+import React, { useState } from "react";
+import { useNavigate } from "react-router";
+import { Get, Post } from "../../scripts";
+import { Buffer } from "buffer";
 
+function CreateProject() {
+  const [image, setImage] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [mainImage, setMainImage] = useState(null);
 
+  const uploadImage = (e) => {
+    console.log(e.target.files);
+    setImage(e.target.files[0]);
+  };
 
+  const handleApi = async (e) => {
+    setLoading(true);
+    const formData = new FormData();
+    formData.append("image", image);
+    Post("/images", formData)
+      .then((res) => {
+        Get(`/images/${res}`).then((response) => {
+          console.log(response);
+          setMainImage(
+            "data:image/png;base64," +
+              Buffer.from(response.data).toString("base64")
+          );
+          setLoading(false);
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-function CreateProject() 
-{
-  
-  const [image, setImage] = useState('');
-  const [ loading, setLoading ] = useState( false );
-  
-  const uploadImage = async e =>
-  {
-    const files = e.target.files
-    const data = new FormData()
-    data.append( 'file', files[ 0 ] )
-    data.append( 'upload_preset', 'darwin' )
-    setLoading( true )
-    const res = await fetch(
-      'http://localhost:4000/image/upload',
-      {
-        method: 'POST',
-        body: data
-      }
-    
-    )
-    const file = await res.json()
-
-    setImage( file.secure_url )
-    setLoading(false)
-  }
- 
   return (
     <div className="create-project">
       <h1>Upload Image</h1>
-      <input type="file"
-        name="file"
-        placeholder="Upload an image"
-        onChange={uploadImage}
-      />
-
       {loading ? (
         <h3>Loading...</h3>
       ) : (
-        <img src={image} style={{ width: '300px' }} />
+        <img src={mainImage ? mainImage : ""} style={{ width: "300px" }} />
       )}
-
+      <input type="file" onChange={uploadImage} />
+      <button onClick={handleApi}> SUBMIT </button>
     </div>
-  )
+  );
 }
 export default CreateProject;
