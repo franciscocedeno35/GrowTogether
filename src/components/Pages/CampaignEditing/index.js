f07094817602669b852dfdf0039cc6ec1ba03ed1
birdsnbears <louischu15@gmail.com>
2022-12-08 21:23:00 -0800

    Fixed index.js file

--- src/components/Pages/CampaignEditing/index.js
+++ src/components/Pages/CampaignEditing/index.js
@@ -1,8 +1,8 @@
-import { CampaignOverview } from "./CampaignOverview";
-import { ContentEditor } from "./ContentEditor";
-import { EditCampaignSettings } from "./EditCampaignSettings";
-import { EditContent } from "./EditContent";
-import { EditRewards } from "./EditRewards";
-import { RewardEditor } from "./RewardEditor";
+import CampaignOverview from "./CampaignOverview";
+import ContentEditor from "./ContentEditor";
+import EditCampaignSettings from "./EditCampaignSettings";
+import EditContent from "./EditContent";
+import EditRewards from "./EditRewards";
+import RewardEditor from "./RewardEditor";
 
 export { CampaignOverview, ContentEditor, EditCampaignSettings, EditContent, EditRewards, RewardEditor };
