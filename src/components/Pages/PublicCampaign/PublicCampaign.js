1ef67561240f3aa812bfb2b65f41a92328c43bb8
aliamirhassani <Ali.Amirhassani1@gmail.com>
2022-12-14 00:43:44 -0800

    Fixed Public campaign positioning

--- src/components/Pages/PublicCampaign/PublicCampaign.js
+++ src/components/Pages/PublicCampaign/PublicCampaign.js
@@ -85,55 +85,61 @@ const PublicCampaign = () => {
   };
 
   return (
-    <div>
-      <div className="title-section">
-        <h1 className="campaign-title">{CampInfo.title}</h1>
-        <h4>{CampInfo.subtitle}</h4>
-      </div>
-      <div className="intro-block">
-        <img src={mainImage ? mainImage : ""} />
-        <div className="intro-stats">
-          <hr />
-          <div className="stat-block">
-            <div>
-              <h1 className="intro-main-stat green-highlight">${CampInfo.currentlyDonated.toLocaleString("en")}</h1>
-              <h6 className="intro-stat-label">pledged of ${CampInfo.goal.toLocaleString("en")} goal</h6>
-            </div>
-            <div>
-              <h1 className="intro-main-stat">{CampInfo.backerNum}</h1>
-              <h6 className="intro-stat-label">backers</h6>
-            </div>
-            <div>
-              <h1 className="intro-main-stat">{CampInfo.daysToGo}</h1>
-              <h6 className="intro-stat-label">days to go</h6>
-            </div>
-          </div>
-          <button className="donate-button" onClick={goToDonate}>
-            Back This Project
-          </button>
-          <hr />
-        </div>
-      </div>
-      <div className="bottom-section">
-        <div className="content-section">
-          {CampInfo.content.map((content, i) => (
-            <div key={i}>
-              <PublicContent content={content} />
-            </div>
-          ))}
-        </div>
-        <div className="rewards-section">
-          <h1 className="title-section reward-title">Rewards</h1>
-          <div className="rewards-list">
-            {CampInfo.rewards.map((reward) => (
-              <div key={reward._id}>
-                <PublicReward reward={reward} />
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+		<div>
+			<div className="title-section">
+				<h1 className="campaign-title">{CampInfo.title}</h1>
+				<h4>{CampInfo.subtitle}</h4>
+			</div>
+			<div className="intro-block">
+				<img src={mainImage ? mainImage : ''} />
+				<div className="intro-stats">
+					<hr />
+					<div className="stat-block">
+						<div>
+							<h1 className="intro-main-stat green-highlight">
+								${CampInfo.currentlyDonated.toLocaleString('en')}
+							</h1>
+							<h6 className="intro-stat-label">
+								pledged of ${CampInfo.goal.toLocaleString('en')} goal
+							</h6>
+						</div>
+						<div>
+							<h1 className="intro-main-stat">{CampInfo.backerNum}</h1>
+							<h6 className="intro-stat-label">backers</h6>
+						</div>
+						<div>
+							<h1 className="intro-main-stat">{CampInfo.daysToGo}</h1>
+							<h6 className="intro-stat-label">days to go</h6>
+						</div>
+					</div>
+					<button className="donate-button" onClick={goToDonate}>
+						Back This Project
+					</button>
+					<hr />
+				</div>
+			</div>
+			<div className="bottom-section">
+				<div className="child content-section">
+					{CampInfo.content.map((content, i) => (
+						<div key={i}>
+							<PublicContent content={content} />
+						</div>
+					))}
+				</div>
+				<div className="sticky">
+					<div className="child rewards-section">
+						<h1 className="title-section reward-title">Rewards</h1>
+						<div className="rewards-list">
+							{CampInfo.rewards.map((reward) => (
+								<div key={reward._id}>
+									<PublicReward reward={reward} />
+								</div>
+							))}
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 export default PublicCampaign;
